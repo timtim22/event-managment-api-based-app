e15554ae6759e097554b871f0fb42a2752ae4dee
Kamran Ali <kami.uqabi@gmail.com>
2020-09-08 17:29:21 +0500

    remove reply migrations

--- db/migrate/20191224163329_add_column_to_replies.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToReplies < ActiveRecord::Migration[5.2]
-  def change
-    add_column :replies, :from, :string
-  end
-end
--- db/migrate/20200810110508_change_schema_of_replies.rb
+++
@@ -1,7 +0,0 @@
-class ChangeSchemaOfReplies < ActiveRecord::Migration[5.2]
-  def change
-     remove_column :replies, :from
-     change_column :replies, :msg, :message
-     add_column :replies, :user_id, :integer
-  end
-end
