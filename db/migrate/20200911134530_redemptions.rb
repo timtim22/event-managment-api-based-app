c31ae7122f75c1f77be9cd94130d5ab548ba7b4a
Kamran Ali <kami.uqabi@gmail.com>
2020-09-11 19:05:26 +0500

    migrate fix

--- db/migrate/20200911134530_redemptions.rb
+++ db/migrate/20200911134530_redemptions.rb
@@ -3,6 +3,7 @@ class Redemptions < ActiveRecord::Migration[5.2]
     create_table :redemptions do |t|
       t.integer :user_id
       t.integer :offer_id
+      t.string :offer_type
       t.integer :code, default: 0
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_11_114743) do
+ActiveRecord::Schema.define(version: 2020_09_11_134530) do
 
   create_table "activity_logs", force: :cascade do |t|
     t.integer "user_id"
@@ -371,6 +371,7 @@ ActiveRecord::Schema.define(version: 2020_09_11_114743) do
   create_table "redemptions", force: :cascade do |t|
     t.integer "user_id"
     t.integer "offer_id"
+    t.string "offer_type"
     t.integer "code", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
