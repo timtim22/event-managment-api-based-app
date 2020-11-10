1c27b2917838fc03070b04a19367c9f795a367c7
timtim <tempetationx@hotmail.com>
2020-11-10 15:31:10 +0500

    fixed production email notification for new user registration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,23 +96,23 @@ Rails.application.configure do
   #   arguments: '-i'
   # }
  
-  config.action_mailer.default_options = {from: 'kamran.ali@mygo.io'}
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors= true
+  config.action_mailer.default_options = {from: 'no-reply@mygo.io'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.mailtrap.io',
-    port:                  2525,
-    domain:               'https://mygo.io',
-    user_name:            ENV['MAILTRAP_USERNAME'],
-    password:             ENV['MAILTRAP_PASSWORD'],
-    authentication:       'cram_md5',
-    enable_starttls_auto: true  }
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'smtp.gmail.com',
+    user_name:            'dev.team@mygo.io',
+    password:             'gneseduhasuqdvdv',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    openssl_verify_mode: "none",
 
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+      }
 end
