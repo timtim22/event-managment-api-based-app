7516fb90c1d18b9ad9d34298d8a48d280483f083
taimoor <taimoor.hassan@mygo.io>
2020-11-27 20:13:13 +0500

    minor changes

--- app/controllers/api/v1/friendships_controller.rb
+++ app/controllers/api/v1/friendships_controller.rb
@@ -209,11 +209,9 @@ def accept_request
        message: "Friend request accepted.",
        data: {
         friend_id: request.friend_id,
-        friend_name: get_full_name(request.friend),
         friend_name: request.friend.profile.first_name,
         last_name: request.friend.profile.last_name,
         avatar: request.friend.avatar,
-        mutual_friends_count: get_mutual_friends(request_user, request).count,
         is_ambassador: request.friend.profile.is_ambassador
 
        }
