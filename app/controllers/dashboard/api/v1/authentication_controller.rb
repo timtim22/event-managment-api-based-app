1f0500138d01da977ccd9e8841fab939c680c081
taimoor <taimoor.hassan@mygo.io>
2020-12-03 15:37:12 +0500

    added API keys in login for dashboard user

--- app/controllers/dashboard/api/v1/authentication_controller.rb
+++ app/controllers/dashboard/api/v1/authentication_controller.rb
@@ -1,20 +1,20 @@
-class Dashboard::Api::V1::AuthenticationController < Dashboard::Api::V1::ApiMasterController 
+class Dashboard::Api::V1::AuthenticationController < Dashboard::Api::V1::ApiMasterController
   before_action :authorize_request, only: [:update_password]
-  
+
    # POST /auth/login
    def login
-    
+
     if params[:email].blank? == true
-      render json: { 
+      render json: {
         code: 400,
         success: false,
         message: "Email is required.",
         data: nil
        }
 
-      elsif params[:password].blank? == true 
+      elsif params[:password].blank? == true
         render json: {
-          code: 400, 
+          code: 400,
           success: false,
           message: "Password is required.",
           data: nil
@@ -22,22 +22,34 @@ class Dashboard::Api::V1::AuthenticationController < Dashboard::Api::V1::ApiMast
     else
       @user = User.authenticate(params[:email], params[:password])
     if @user
+          social_links = {
+            "youtube" => @user.business_profile.youtube,
+            "facebook" => @user.business_profile.facebook,
+            "instagram" => @user.business_profile.instagram,
+            "twitter" => @user.business_profile.twitter,
+            "linkedin" => @user.business_profile.linkedin
+          }
+
+
       user_data = {}
       user_data['id'] =  @user.id,
-      user_data['profile_name'] =  @user.business_profile.profile_name,
       user_data['avatar'] = @user.avatar,
       user_data['email'] = @user.email,
-      user_data['contact_name']  = @user.business_profile.contact_name,
       user_data['address'] = @user.business_profile.address,
       user_data['vat_number'] = @user.business_profile.vat_number,
       user_data['charity_number']  = @user.business_profile.charity_number,
       user_data['website'] =  @user.business_profile.website,
       user_data['about'] =  @user.business_profile.about,
+      user_data['phone_number']  = @user.phone_number,
+      user_data['profile_name']  = @user.business_profile.profile_name,
+      user_data['contact_name']  = @user.business_profile.contact_name,
+      user_data['display_name']  = @user.business_profile.display_name,
+      user_data['social_links']  = social_links,
       user_data['phone_number']  = @user.phone_number
       # create_activity creates login issue regarding jwt auth token requirements
       #create_activity('logged in.', @user, 'User', '', '', 'post')
       token = encode(user_id: @user.id)
-      render json: { 
+      render json: {
             code: 200,
             success: true,
             message: "Login is successful.",
@@ -47,7 +59,7 @@ class Dashboard::Api::V1::AuthenticationController < Dashboard::Api::V1::ApiMast
             }
           }
      else
-      render json: { 
+      render json: {
         code: 401,
         success: false,
         message: "Email/password is incorrect.",
@@ -57,5 +69,5 @@ class Dashboard::Api::V1::AuthenticationController < Dashboard::Api::V1::ApiMast
   end
 
   end
-    
+
 end
