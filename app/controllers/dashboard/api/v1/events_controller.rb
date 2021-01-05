e215cd705cd9f6d4d5edb5277891d4b40e3598e7
taimoor <taimoor.hassan@Mygo.io>
2021-01-05 19:14:02 +0500

    update event

--- app/controllers/dashboard/api/v1/events_controller.rb
+++ app/controllers/dashboard/api/v1/events_controller.rb
@@ -84,7 +84,15 @@ class Dashboard::Api::V1::EventsController < Dashboard::Api::V1::ApiMasterContro
      'creator_image' => e.user.avatar,
      'event_status' => e.status,
      'event_type' => e.event_type,
-     'event_type' => e.over_18
+     'event_type' => e.over_18,
+      "event_dates" => e.child_events.map {|ch| 
+        {
+          id: ch.id,
+          start_date: ch.start_date,
+          end_date: ch.end_date
+        }
+
+      }
 
   }
 
