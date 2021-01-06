e25de67789acb1b8ededa858549b2260909ddc36
Kamran Ali <kami.uqabi@gmail.com>
2021-01-06 17:14:07 +0500

    fixes

--- app/controllers/api/v1/business_dashboard_controller.rb
+++ app/controllers/api/v1/business_dashboard_controller.rb
@@ -182,12 +182,12 @@ class Api::V1::BusinessDashboardController < Api::V1::ApiMasterController
   def show_event
     if !params[:event_id].blank?
         child_event = ChildEvent.find(params[:event_id])
-        e = child_event.event
+        e = child_event
           @passes = []
           @ticket = []
           all_pass_added = false
           if request_user
-            all_pass_added = has_passes?(.event) && all_passes_added_to_wallet?(request_user, e.passes)
+            all_pass_added = has_passes?(e.event) && all_passes_added_to_wallet?(request_user, e.event.passes)
           e.event.passes.not_expired.map { |pass|
           if !is_removed_pass?(request_user, pass)
             @passes << {
