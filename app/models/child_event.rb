063ccf60ea8660ba4ba25c9a041b1ebc4f7233e8
taimoor <taimoor.hassan@Mygo.io>
2021-01-12 16:38:36 +0500

    updated

--- app/models/child_event.rb
+++ app/models/child_event.rb
@@ -1,7 +1,7 @@
 class ChildEvent < ApplicationRecord
  belongs_to :event
  belongs_to :user
- scope :not_expired, -> { where(['end_date > ?', DateTime.now]) }
+ scope :not_expired, -> { where(['end_time > ?', DateTime.now]) }
  scope :sort_by_date, -> { order(start_date: 'ASC') }
  mount_uploader :image, ImageUploader
  mount_base64_uploader :image, ImageUploader
