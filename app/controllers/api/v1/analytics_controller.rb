19b817b0d5d549bcf6e1debc84cfe804f227f59f
Kamran Ali <kami.uqabi@gmail.com>
2021-01-22 21:31:14 +0500

    typo

--- app/controllers/api/v1/analytics_controller.rb
+++ app/controllers/api/v1/analytics_controller.rb
@@ -982,7 +982,7 @@ def get_time_slot_special_offers_increment_decrement(current_time_slot_dates,   
         movement_percent_now =   get_percent_of(current_size, competition.registrations.size)
     
         differenct_in_movement_percent =  movement_percent_now - movement_percent_before 
-        differenct_in_movement_percent.rount(2)
+        differenct_in_movement_percent.round(2)
      
       end
 
