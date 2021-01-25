9d6a5d1faf69733a3131f8a6e150f19fd4b7c879
taimoor <taimoor.hassan@Mygo.io>
2021-01-25 11:09:56 +0500

    fixed bug in my attending

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,5 +1,5 @@
 class Reminder < ApplicationRecord
   belongs_to :user
-  belongs_to :event, optional: true,
-  belongs_to :child_event, optional: true,
+  belongs_to :event, optional: true
+  belongs_to :child_event, optional: true
 end
