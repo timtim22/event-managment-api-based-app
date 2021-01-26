8bd7f0adf0facf41e8d0e59876d9d6be91c7e691
taimoor <taimoor.hassan@Mygo.io>
2021-01-26 16:49:19 +0500

    made some changes in wallet

--- app/models/pass.rb
+++ app/models/pass.rb
@@ -11,6 +11,7 @@ class Pass < ApplicationRecord
   has_many :viewers, through: :views, source: :user
   has_many :vip_pass_shares, dependent: :destroy
   has_many :notifications, dependent: :destroy, as: :resource
+  has_many :activity_logs, dependent: :destroy, as: :resource
 
 
   validates :title, presence: true
