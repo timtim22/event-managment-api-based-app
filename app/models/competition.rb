72dd0f8fff0dd90f498e0433dfbf1d59d4e9e94f
taimoor <taimoor.hassan@Mygo.io>
2021-02-22 13:20:06 +0500

    removed start and end time from competition

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -16,8 +16,6 @@ class Competition < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
 
-  validates :start_time, presence: true
-  validates :end_time, presence: true
   validates :description, presence: true
  
 
