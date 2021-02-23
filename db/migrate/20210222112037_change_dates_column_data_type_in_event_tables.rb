15a51258b168f026af4083cec741297e7c17b6c7
taimoor <taimoor.hassan@Mygo.io>
2021-02-23 13:28:41 +0500

    fixed migration

--- db/migrate/20210222112037_change_dates_column_data_type_in_event_tables.rb
+++
@@ -1,8 +0,0 @@
-class ChangeDatesColumnDataTypeInEventTables < ActiveRecord::Migration[5.2]
-  def change
- 	change_column :events, :start_date, :datetime
- 	change_column :events, :end_date, :datetime
- 	change_column :child_events, :start_date, :datetime
- 	change_column :child_events, :end_date, :datetime
-  end
-end
