17c1b8f694536ec99bf3cbf4076de263d6d70303
Kamran Ali <kamran.ali@mygo.io>
2021-03-03 12:46:23 +0500

    association and fixes

--- app/controllers/api/v1/events/tickets/reservations_controller.rb
+++ app/controllers/api/v1/events/tickets/reservations_controller.rb
@@ -12,7 +12,7 @@ class Api::V1::Events::Tickets::ReservationsController < Api::V1::ApiMasterContr
     def create
         if !params[:ticket_id].blank? && !params[:user_id].blank?
              user = User.find(params[:user_id])
-             ticket = Ticket.find(params(ticket_id))
+             ticket = Ticket.find(params[:ticket_id])
             if reservation = user.reservations.create!(ticket: ticket, start_time: params[:start_time], end_time: params[:end_time])
                 render json: {
                 code: 200,
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,5 @@
 class Reservation < ApplicationRecord
    belongs_to :reserved_for, foreign_key: :reserved_for_id, class_name: "User"
    belongs_to :user
+   belongs_to :ticket
 end
